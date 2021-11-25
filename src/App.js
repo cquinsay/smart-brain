f62a848805b93b919dc085f769beb6c36b190489
Charity Quinsay <cquinsay@gmail.com>
2021-11-24 23:20:19 -0800

    added logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
+
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Logo />
 
     </div>
   );
